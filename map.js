2ed490d82055c7382f6045fe5ae97a3e6befd7be
csaphi <csaphi@mit.edu>
2022-10-31 10:54:14 -0400

    messag

---
+++ map.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = "pk.eyJ1IjoiY3NhcGhpIiwiYSI6ImNsOXd0eXJ5aDA0MXozcm1qbTl0Z3dldnkifQ.NZxoecH0XDE2h-SOyCxeEQ";
+const map = new mapboxgl.Map({
+    container:"map",
+    style:"mapbox://styles/csaphi/cl9wu1dto000214qh4jfaga4y",
+    center: [39.988,-75.172],
+    zoom: 11.42,
+    projection: "globe",
+});
